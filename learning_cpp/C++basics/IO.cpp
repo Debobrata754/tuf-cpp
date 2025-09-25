c6d83416d8e5a995695fe7f7c27b27686adf2dac
Debobrata Paul <debobratap076@gmail.com>
2025-09-25 18:19:19 +0530

    basics of c++

---
+++ learning_cpp/C++basics/IO.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    string str;
+    cin >> n;
+    getline(cin, str);
+    cout << n << endl;
+    cout << str << endl;
+    return 0;
+}
